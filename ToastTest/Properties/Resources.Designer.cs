be1802af65132fd2f1311876549cbfec5f2542ca
Anthonyrules144 <epicman56@yahoo.com>
2017-10-09 05:43:42 -0500

    1.1.8.3 wtf

--- ToastTest/Properties/Resources.Designer.cs
+++ ToastTest/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ToastTest.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon spotify_icon {
-            get {
-                object obj = ResourceManager.GetObject("spotify_icon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
